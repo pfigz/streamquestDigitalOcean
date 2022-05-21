3cb0af9b02efe68dc20cafa902630694c132d1a1
pfigz <pfigdev@gmail.com>
2022-05-22 00:40:38 +0200

    Update styling and position for search-container-selection component

--- resources/js/Pages/Search/SearchContainerResults.vue
+++ resources/js/Pages/Search/SearchContainerResults.vue
@@ -1,7 +1,7 @@
 <template>   
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-start flex-wrap m-auto">
-            <div v-for="result in results.results" :key="result">              
+            <div v-for="result in results.results" :key="result">            
                 <search-container-result-item 
                     :resultName="result.name" 
                     :resultType="result.type" 
@@ -30,7 +30,7 @@ export default {
         selectTitle(resultId) {
             this.$emit('selectTitle', resultId);
         }
-    }
+    },
 }
 </script>
 
--- resources/js/Pages/Search/SearchContainerSelection.vue
+++ resources/js/Pages/Search/SearchContainerSelection.vue
@@ -1,13 +1,14 @@
 <template>
+<div class="flex justify-center max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div
         class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg"
     >
         <img
-            class="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+            class="w-full mr-2 h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
             :src="`${selection.poster}`"
             alt=""
         />
-        <div class="p-6 flex flex-col justify-start">
+        <div class="flex flex-col justify-start">
             <h5 class="text-gray-900 text-xl font-medium mb-2">
                 {{ selection.title }}
             </h5>
@@ -30,6 +31,7 @@
             </p>
         </div>
     </div>
+</div>
 </template>
 
 <script>
