28ad5162675994d6d2bdffeba801579736bc4732
pfigz <pfigdev@gmail.com>
2022-05-30 21:42:24 +0200

    Add statement for failed searches

--- resources/js/Pages/Search/SearchContainerResults.vue
+++ resources/js/Pages/Search/SearchContainerResults.vue
@@ -1,6 +1,6 @@
 <template>   
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="grid grid-cols-5">
+        <div class="grid grid-cols-5 place-content-center">
             <template v-for="result in results.results" :key="result">            
                 <div v-if="result.image_url != null">
                     <search-container-result-item
@@ -13,6 +13,10 @@
                     />
                 </div>
             </template>
+            
+        </div>
+        <div v-if="results.results == ''" class="flex justify-center">
+            <p class="text-red-600 text-center text-xs md:text-base lg:text-lg">Your quest has failed. Please try again.</p>
         </div>
     </div>
 </template>
@@ -31,8 +35,14 @@ export default {
     methods: {
         selectTitle(resultId) {
             this.$emit('selectTitle', resultId);
-        }
+        },
     },
 }
 </script>
+
+<style scoped>
+p {
+font-family: 'Press Start 2P', cursive;
+}
+</style>
 
