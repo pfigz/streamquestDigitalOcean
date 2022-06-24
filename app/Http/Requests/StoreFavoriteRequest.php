956ace04d77ec18722163808c6f955be61c18423
pfigz <pfigdev@gmail.com>
2022-06-24 16:05:53 +0200

    Add FormRequest validation classes for updating and storing Favorites

--- app/Http/Requests/StoreFavoriteRequest.php
+++ app/Http/Requests/StoreFavoriteRequest.php
@@ -13,7 +13,7 @@ class StoreFavoriteRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdateFavoriteRequest.php
+++ app/Http/Requests/UpdateFavoriteRequest.php
@@ -13,7 +13,7 @@ class UpdateFavoriteRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
