12a4eb2a8bd2d7d2a433441a6910ac2d070768df
pfigz <pfigdev@gmail.com>
2022-06-24 15:21:09 +0200

    Create IsRegistered middleware for future filtering options

---
+++ app/Http/Middleware/IsRegistered.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsRegistered
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->user()?->is_registered == 1) {
+            return $next($request); 
+        }
+        abort(Response::HTTP_FORBIDDEN);
+    }
+}
