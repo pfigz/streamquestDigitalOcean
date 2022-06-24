c21953decfd84a3cd3518c57c014ae332c13283a
pfigz <pfigdev@gmail.com>
2022-06-24 16:07:56 +0200

    Add one to many relationship between User and Favorite models

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function favorites() 
+    {
+        $this->hasMany(Favorite::class, 'user_id');
+    }
 }
