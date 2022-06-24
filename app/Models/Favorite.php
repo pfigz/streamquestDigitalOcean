0e00bbb7c3575f56599f572d31031b9f77b7a0ab
pfigz <pfigdev@gmail.com>
2022-06-24 16:07:04 +0200

    Add relationship and fillable traits to Favorite model

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     use HasFactory;
+
+    /**
+     * Define one to one or many class relationship
+     *
+     * @return void
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'title', 
+        'title_id', 
+        'backdrop_url', 
+        'plot_overview', 
+        'sources',
+    ];
+
 }
