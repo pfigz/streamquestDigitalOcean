69c3417af98f9f94ef98c3501b92fb316290f724
pfigz <pfigdev@gmail.com>
2022-07-25 00:30:06 +0200

    Add fontawesome icons for use in favorite form components and Dashboard views

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,9 +12,24 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mixin({ methods: { route } })
             .mount(el);
     },
 });
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as fasStar} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStar, fasStar)
+
+
 
 InertiaProgress.init({ color: '#4B5563' });
